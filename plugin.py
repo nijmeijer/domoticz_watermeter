fc52965a6c02886734329bfd8418aa5f843c9ac2
Alex Nijmeijer <alex.nijmeijer@neads.nl>
2022-12-02 09:22:19 +0100

    init flow at 0

--- plugin.py
+++ plugin.py
@@ -1,5 +1,5 @@
 """
-<plugin key="Watermeter2" name="Watermeter Sensor2" author="nijmeijer" version="1.0.2">
+<plugin key="Watermeter2" name="Watermeter Sensor2" author="nijmeijer" version="1.0.4">
     <params>
         <param field="Address" label="IP Address" width="200px" required="true" default="192.168.1.86"/>
         <param field="Port" label="Port" width="50px" required="true" default="5001"/>
@@ -25,6 +25,11 @@ class BasePlugin:
         self.pollCount = 0
         self.PrevSample = 0
         self.IdleCount = 0
+        self.prevtime = 0
+        self.LastIncrementAge = 0
+        self.prevtime = 0
+        self.PosFlowDurationSec = 0
+
         return
 
     def onStart(self):
@@ -34,21 +39,43 @@ class BasePlugin:
 
         if (len(Devices) == 0):
             Domoticz.Device(Name="WaterMeter Neads2", Unit=1, TypeName="Counter Incremental", Type=243, Subtype=28, Used=1).Create()
+            Domoticz.Device(Name="WaterMeter Neads2 - Flow", Unit=2, TypeName="Waterflow", Type=243, Subtype=30, Used=0).Create()
+            #Domoticz.Device(Name="WaterMeter Duration Min", Unit=3,  Type=243, Subtype=28, Switchtype=5, Used=0).Create() # counter incremental, Time
 
-        Domoticz.Debug("Device created.")
+        if (len(Devices) == 1):
+            Domoticz.Device(Name="WaterMeter Neads2 - Flow", Unit=2, Type=243, Subtype=30, Used=0).Create()
+            #Domoticz.Device(Name="WaterMeter Duration Min", Unit=3,  Type=243, Subtype=28, Switchtype=5, Used=0).Create() # counter incremental, Time
+
+        if (len(Devices) == 2):
+            Domoticz.Device(Name="WaterMeter Duration Min", Unit=3,  Type=243, Subtype=28, Switchtype=5, Used=0).Create() # counter incremental, Time
+            #pass
+
+        UpdateDevice(3,0,-1) # initialize timer svalue=neg
+        Domoticz.Debug("Devices created.")
         DumpConfigToLog()
 
         self.pollPeriod = 1 * int(Parameters["Mode2"])
         self.pollCount = self.pollPeriod - 1
 
         self.watermeterapi = watermeter2(Parameters['Address'],Parameters['Port'])
+        self.watermeterapi.open_socket()
 
-        Domoticz.Heartbeat(2)
+        self.PosFlowDurationSec = 0
+        self.prevtime = 0
+
+        # Initialize flow to 0
+        UpdateDevice(2,0,0)
+        # increment duration by 0
+        UpdateDevice(3,0,0) 
+
+        Domoticz.Heartbeat(10)
 
 
 
     def onStop(self):
         Domoticz.Debug("onStop called")
+        self.watermeterapi.close_socket()
+        self.isConnected = False
 
     def onConnect(self, Connection, Status, Description):
         Domoticz.Debug("onConnect called")
@@ -66,26 +93,53 @@ class BasePlugin:
 
     def onDisconnect(self, Connection):
         Domoticz.Log("onDisconnect called")
-        self.isConnected = False 
+        self.watermeterapi.close_socket()
+        self.isConnected = False
 
     def onHeartbeat(self):
+        duration_between_updates = 0
+        flow = 0
         # Domoticz.Debug("onHeartBeat called:"+str(self.pollCount)+"/"+str(self.pollPeriod))
         if self.pollCount >= self.pollPeriod*30: #PollPeriod is 0...n minutes, PollCount increments every 2 secs
             #watermeterapi = watermeter(Parameters['Address'],Parameters['Port'])
             #self.watermeterapi = watermeter2(Parameters['Address'],Parameters['Port'])
 
             if 1==1 :
-                curmeas = self.watermeterapi.request_info()
-                Domoticz.Debug("watermeter2 plugin received: " + repr(curmeas))
-                # Domoticz.Debug("watermeter2 current value: " + repr(Devices[1].nValue))
-                Domoticz.Debug("watermeter2 my prevsample: " + repr(self.PrevSample))
+                curmeas, currenttime = self.watermeterapi.request_info()
+                Domoticz.Debug("watermeter2 flow current/prev time: " + repr(currenttime) + "/" +repr(self.prevtime))
+
+                if self.prevtime>0 : #and (currenttime - self.prevtime) > 1000 :  # flow=0 when time unchanged
+                  try :
+                     duration_between_updates = (currenttime -  self.prevtime)
+                     flow=round(60*1000/(currenttime -  self.prevtime)) # ms/tick -> liters/min
+                     UpdateDevice(2,0,flow)
+                  except :
+                    flow=0
+                    duration_between_updates = 0
+                else :
+                  flow=0
+
+                #last increment is too long ago, assume flow=0
+                Domoticz.Debug("watermeter2 flow timeout check ")
+                if (self.LastIncrementAge > 60/2 ) : #   one minute -> #heartbeats
+                  flow=0
+                  Domoticz.Debug("watermeter2 flow set to 0 because of timeout " + repr(self.LastIncrementAge) )
+                  UpdateDevice(2,0,flow)
+
+                if currenttime > 0 :
+                  self.prevtime=currenttime
+
+                if (self.PrevSample != curmeas) :
+                  self.LastIncrementAge = 0
+                self.LastIncrementAge +=  1
+
+                #Domoticz.Debug("watermeter2 plugin received: " + repr(curmeas))
+                Domoticz.Debug("watermeter2 my prevsample: " + repr(self.PrevSample) + " new received: " + repr(curmeas))
                 # if we are sure that we have a valid increment, pass it on to Domoticz
                 if self.PrevSample>0 and curmeas > 0 and curmeas>self.PrevSample :
                   #if curmeas > 0 and curmeas>self.PrevSample :
                   newval = Devices[1].nValue + curmeas - self.PrevSample
-                  Domoticz.Debug("Updating device")
                   UpdateDevice(1,(curmeas - self.PrevSample),(curmeas - self.PrevSample))
-                  Domoticz.Debug("Device updated")
                   self.PrevSample=curmeas
                   self.IdleCount = 0
                 else :
@@ -93,6 +147,7 @@ class BasePlugin:
                    self.PrevSample=curmeas
             self.pollCount = 0 #Reset Pollcount
         else:
+            # not polling
             self.pollCount += 1
 
         # Update Anyway, even if the counter stands still.
@@ -101,7 +156,10 @@ class BasePlugin:
         if self.IdleCount > (5-1) :
             UpdateDevice(1,0,0)
             self.IdleCount = 0
- 
+
+        # keep track of time for a flow>0
+        if flow > 0 :
+           UpdateDevice(3,0,duration_between_updates/1000) # increment by 2 seconds
 
 
 global _plugin
@@ -140,14 +198,26 @@ def onHeartbeat():
     _plugin.onHeartbeat()
 
 def UpdateDevice(Unit, nValue, sValue):
-    # Make sure that the Domoticz device still exists (they can be deleted) before updating it 
+    # Make sure that the Domoticz device still exists (they can be deleted) before updating it
     if (Unit in Devices):
         if (Devices[Unit].nValue != nValue) or (Devices[Unit].sValue != sValue):
             Devices[Unit].Update(nValue, str(sValue))
             Domoticz.Debug("Update "+str(nValue)+":'"+str(sValue)+"' ("+Devices[Unit].Name+")")
     return
 
-    # Generic helper functions
+#def UpdateDevice_sonly(Unit, sValue):
+#  # Make sure that the Domoticz device still exists (they can be deleted) before updating it
+#  try:
+#    a=print("{0:f}".format(sValue))
+#    if (Unit in Devices):
+#         Devices[Unit].Update(0, a)
+#         Domoticz.Debug("Update "+a+" (" + Devices[Unit].Name+")")
+#    return
+#  except:
+#    pass
+
+
+# Generic helper functions
 def DumpConfigToLog():
     for x in Parameters:
         if Parameters[x] != "":
